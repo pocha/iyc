013255876e341c7a2fad89fc40deeeeff4ed95ab
Ashish Sharma <ashish@catapultlabs.xyz>
2025-08-12 12:14:34 +0530

    comment edit flow was not disabling edit & delete buttons .. it is because the edit url is in effect but we store the post url in workflow tracker. Hence added page reload in setTimeout instead of closing the popup

--- assets/js/comment-manager.js
+++ assets/js/comment-manager.js
@@ -114,7 +114,8 @@ document.getElementById("editCommentForm").addEventListener("submit", async func
       statusDiv.className = "mb-4"
 
       setTimeout(() => {
-        closeEditFormModal()
+        // closeEditFormModal()
+        window.location.reload() // reloading as the edit & delete button will not be disabled by simply closing the modal
       }, 2000)
     } else {
       throw new Error(data.error || "Failed to update comment")
